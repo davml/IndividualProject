4e824b47f3a7e84a66efb44537402b2c2320816a
gvb17187 <david.lewis.2017@uni.strath.ac.uk>
2021-01-30 22:39:07 +0000

    Further Job Cards Implementation

--- src/components/pages/JobsSearch/JobItem/JobItem.js
+++ src/components/pages/JobsSearch/JobItem/JobItem.js
@@ -1,14 +1,36 @@
 import React, { useState } from 'react';
 import classes from './JobItem.css';
-import image from '../../../../assets/images/coding.jpg';
+import image from '../../../../assets/images/coder.jpg';
 
 const JobItem = (props) => {
     return (
         <>
             <div className={classes["item-wrapper"]}>
                 <div className={classes["job-item"]}>
-                    <div className={classes["job-info"]}>
-                        <div className={classes["job-heading"]}>Job Heading</div>
+                    <div className={classes["job-img"]}>
+                        <img src={image} className={classes["image"]}/>
+                        <div className={classes["overlay-text-1"]}>
+                            Software Developer
+                        </div>
+                        <div className={classes["overlay-text-2"]}>
+                            <p>
+                                <i class="far fa-money-bill-alt"></i> £250
+                                <br/>
+                                <i class="far fa-clock"></i> 16/03/2021
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={classes["job-description"]}>
+                        <div className={classes["heading"]}>
+                            Description:
+                        </div>
+                        <div className={classes["description"]}>
+                            Software Engineer required for my client, an Oil and Gas linked with business based in Aberdeen.
+                        </div>
+                        <div className={classes["more-info"]}>
+                            <button type="submit" className={classes["more-button"]}>Read More</button>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/pages/JobsSearch/JobsSearch.js
+++ src/components/pages/JobsSearch/JobsSearch.js
@@ -8,6 +8,33 @@ const JobsSearch = (props) => {
             <div className={classes["search-wrapper"]}>
                 <div className={classes["jobs-wrapper"]}>
                     <div className={classes["refine-search"]}>
+                        <div className={classes["properties-container"]}>
+                            <div className={classes["query"]}>
+                                <p>Search Results For: <br/><i>Mobile App Developer</i></p>
+                            </div>
+
+                            <div className={classes["properties"]}>
+                                <select name="category" id="category" className={classes["category-dropdown"]}>
+                                    <option value="category-default">Default</option>
+                                    <option value="mobile-apps">Mobile Applications</option>
+                                    <option value="web-apps">Web Applications</option>
+                                    <option value="graphic-design">Graphic Design</option>
+                                </select>
+
+                                <select name="category" id="category" className={classes["category-dropdown"]}>
+                                    <option value="category-default">Default</option>
+                                    <option value="mobile-apps">Mobile Applications</option>
+                                    <option value="web-apps">Web Applications</option>
+                                    <option value="graphic-design">Graphic Design</option>
+                                </select>
+                                <select name="category" id="category" className={classes["category-dropdown"]}>
+                                    <option value="category-default">Default</option>
+                                    <option value="mobile-apps">Mobile Applications</option>
+                                    <option value="web-apps">Web Applications</option>
+                                    <option value="graphic-design">Graphic Design</option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
 
                     <div className={classes["search-results"]}>
@@ -29,7 +56,15 @@ const JobsSearch = (props) => {
                         <div className={classes["job-item"]}>
                             <JobItem />
                         </div>
-                        
+                        <div className={classes["job-item"]}>
+                            <JobItem />
+                        </div>
+                        <div className={classes["job-item"]}>
+                            <JobItem />
+                        </div>
+                        <div className={classes["job-item"]}>
+                            <JobItem />
+                        </div>
                     </div>
                 </div>
             </div>
